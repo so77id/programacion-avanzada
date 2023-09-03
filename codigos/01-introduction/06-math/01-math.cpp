c1e26faed1d6665445a1427772a856bbc6adbc6c
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2023-09-03 11:50:58 -0300

    WIP

--- codigos/01-introduction/06-math/01-math.cpp
+++ codigos/01-introduction/06-math/01-math.cpp
@@ -16,6 +16,9 @@ int main() {
     cout << "La raiz cuadrada de v2 es: " << sqrt(v2) << endl;
     cout << "El valor absoluto de v3 es: " << abs(v3) << endl;
     cout << "El exp(v3) es: " << exp(v3) << endl;
+
+    int v4 = floor(f1);
+    cout << v4 << endl;
     cout <<  "El floor de f1 es: " << floor(f1) << " el floor de f2 es: " << floor(f2) << endl;
     cout <<  "El ceil de f1 es: " << ceil(f1) << " el ceil de f2 es: " << ceil(f2) << endl;
     cout <<  "El round de f1 es: " << round(f1) << " el round de f2 es: " << round(f2) << endl;
